dc666975d6277df2f8d425451df850817bc00711
takiananasek <76755039+takiananasek@users.noreply.github.com>
2022-08-01 23:18:31 +0200

    Code refactoring exercise solution

---
+++ ConsoleApp/Models/ImportedObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Models
+{
+    public class ImportedObject :ImportedObjectBase
+    {
+        public string Schema { get; set; }
+        public string ParentName  { get; set; }
+        public string ParentType { get; set; }
+        public string DataType { get; set; }
+        public string IsNullable { get; set; }
+        public double NumberOfChildren { get; set; }
+    }
+}
